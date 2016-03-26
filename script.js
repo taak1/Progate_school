1f5dae9c0d0baad1f9540651064225b0978c28da
Taku Itashiki <takuitashiki@Takus-MacBook-Air.local>
2016-03-26 10:59:18 +0900

    ok

--- script.js
+++ script.js
@@ -51,4 +51,35 @@ $(function(){
     var position = $(id).offset().top;
     $("html,body").animate({scrollTop: position}, 1000);
   });
+
+// コラムがホバーして拡大暗くなるところ //
+  $('.caption-defaultWrap').hover(
+    function(){
+      $(this).addClass('.zoom');
+      $(this).children('.zoom-black').fadeIn(300);
+    },
+    function(){
+      $(this).removeClass('.zoom');
+      $(this).children('.zoom-black').fadeOut(300);
+    }
+  );
+
+  //  // カルーススライダー部分 //
+  // $('.carousel').carousel({
+  //   interval: 2500
+  // })
+
+// コラム整理
+  $('.filter-item').click(function(){
+    $('.filter-item').removeClass('active');
+    $(this).addClass('active');
+    var id = $(this).attr('id');
+    if (id === 'all') {
+      $('.column-box').fadeIn();
+    } else {
+      $('.column-box-wrapper').children('.' + id).fadeIn();
+      $('.column-box').not('.' + id).fadeOut();
+    }
+  });
+
 });
